eda982704c3bbeee8621286172e269dcf81a2d6d
amin <none>
2022-11-12 20:10:36 +0330

    DONE: 	after_request 	login strracture

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #imports
-from flask import Flask
+from flask import Flask ,request ,redirect ,render_template
 from flask import SQLAlchemy
 from flask_session import Session
 from helpers import db_conf
@@ -18,3 +18,28 @@ Session(app)
 #TODO 
 #config database
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# @app.route("/")
+# @login_requierd   #TODO
+# def index()
+
+#login
+@app.route("/login" ,methods=["GET", "POST"])
+def login():
+    Session.clear()
+    if request.method == "POST":
+        #TODO
+        #Ensure about valid inputs
+
+        #TODO
+        # Remember which user has logged in
+        return redirect("/")
+    else:
+        return render_template("login.html")

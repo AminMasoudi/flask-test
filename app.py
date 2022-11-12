43726a025451ab96cad9659f576453caa947e71b
amin <none>
2022-11-12 23:10:51 +0330

    DONE : 	app/login 	db 	helpers/apology

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #imports
-from flask import Flask ,request ,redirect ,render_template
-from flask import SQLAlchemy
+from flask import Flask ,request ,redirect ,render_template ,session
 from flask_session import Session
-from helpers import db_conf
+from werkzeug.security import check_password_hash
+from helpers import apology 
+import sqlite3 as sql
+
 
 #conf application
 app = Flask(__name__)
@@ -15,8 +17,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-#TODO 
-#config database
+
+#database configuration
+con = sql.connect("flask-test.db",check_same_thread=False)
+con.row_factory = sql.Row
+db = con.cursor()
 
 @app.after_request
 def after_request(response):
@@ -33,13 +38,29 @@ def after_request(response):
 #login
 @app.route("/login" ,methods=["GET", "POST"])
 def login():
-    Session.clear()
+    session.clear()
     if request.method == "POST":
-        #TODO
-        #Ensure about valid inputs
+    #TODO
+    #Ensure about valid inputs
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
 
-        #TODO
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username")).fetchall()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
+            return apology("invalid username and/or password")
+
         # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
         return redirect("/")
     else:
         return render_template("login.html")
